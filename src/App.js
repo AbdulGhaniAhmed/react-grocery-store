b9109e115a16b093df87dc6369fc3a08e7887cdf
Ghani <ghaniahmed951@gmail.com>
2020-12-06 17:30:00 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href='/'>Grocery Mart</a>        
       </header>
+      <main>Product List</main>
+      <footer>Alright Reserved</footer>
     </div>
   );
 }
