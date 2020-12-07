27e072c3d0d4fdd073f7181c98e206c007c5b7a4
Ghani <ghaniahmed951@gmail.com>
2020-12-07 11:24:21 +0500

    cart adding

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import React from 'react';
 import ProductsData from './ProductsData.json';
 import Products from './components/Products';
 import FilterCategory from './components/FilterCategory';
+import Cart from './components/Cart';
 
 class App extends React.Component{
   constructor(){
     super();
     this.state={
       products: ProductsData.products,
-      size:""
-    };
-  }
+      size:"",
+      cartItems: localStorage.getItem("cartItems") ?
+                 JSON.parse(localStorage.getItem("cartItems"))
+                 :[],
+    } 
+  }      
+  addToCart = (product)=>{
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if(item._id === product._id){
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count : 1})
+    }
+   this.setState({cartItems});
+   localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
+  removeFromCart = (product) =>{
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !== product._id)));
+  };
   filterProducts = (event) =>{
     console.log(event.target.value);
     if(event.target.value === ""){
@@ -42,9 +68,11 @@ class App extends React.Component{
             filterProducts={this.filterProducts}
             >
             </FilterCategory>
-            <Products products={this.state.products}></Products>
+            <Products products={this.state.products} addToCart={this.addToCart}> </Products>
             </div>
-          <div className="sideBar">Cart Item</div>
+          <div className="sideBar">
+            <Cart cartItems={this.state.cartItems} removeItem={this.removeFromCart}/>
+          </div>
           </div>
       </main>
       <footer>Alright Reserved</footer>
---
+++ src/components/Cart.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Currency from "../currencyFormat";
+
+export default class Cart extends Component {
+    render() {
+        const {cartItems} = this.props;
+        return (
+            <div>
+                {cartItems.length === 0 ? 
+                (<div className="cart cart-header">Cart is empty</div>)
+                :
+                (<div className="cart cart-header">You have {cartItems.length} items in cart</div>)}
+                <div>
+                    <div className="cart">
+                         <ul className="cart-items">
+                             {cartItems.map(item=>(
+                                 <li key={item._id}>
+                                     <div>
+                                         <img src={item.image} alt={item.title}></img>
+                                     </div>
+                                     <div>
+                                        <div>{item.title}</div>
+                                        <div className="right">
+                                            <div>{Currency(item.price)} x {item.count}{" "}</div>
+                                            <button className="button" onClick={()=>this.props.removeItem(item)}>Remove</button>
+                                        </div>
+                                        
+                                     </div>
+                                 </li>
+                             ))}
+                         </ul>
+                    </div>
+                    {cartItems.length !== 0 && 
+                    (<div className="cart">
+                             <div className="total">
+                                 <div>
+                                     Total:{""},
+                                     {Currency(cartItems.reduce((a,c)=> a + c.price*c.count,0))}
+                                 </div>
+                                 <button className="button-primary">Checkout</button>
+                             </div>
+                         </div>)}
+                    
+                </div>
+            </div>
+        )
+    }
+}
